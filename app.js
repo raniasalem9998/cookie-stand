c474853a41022d4b3ceb84511839de40a3c79a98
raniasalem9998 <rania.salem9998@gmail.com>
2020-06-14 23:09:01 +0300

    class06

--- app.js
+++ app.js
@@ -6,29 +6,30 @@ var seattle = {
     maxCostumerNum: 65,
     avrCookies: 6.3,
     hour: document.querySelectorAll("li"), //cookies in the hour
-    cookiesPerHour=0,
-    totalArray=document.querySelectorAll(".total")[0],
+    totalArray:document.querySelectorAll(".total")[0],
 
     // randum: function () {
     //     this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum);
     // },
-    
+
     total: function () {
         for (var i = 0; i <= this.hour.length; i++) {
-             var allCookies=0;
-            if (i<=6){
-        this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum)*this.avrCookies;
+            var allCookies = 0;
+            var cookiesPerHour=0;
+             if (i <= 6) {
+                this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum) * this.avrCookies;
 
-                hour[i].textContent = (i+6) +'pm:' + cookiesPerHour + 'cookies';
-                  allCookies=allCookies+hour[i];
-                totalArray[0].textContent='Total='+ allCookies;
+                hour[i].textContent = (i + 6) + 'pm:' + cookiesPerHour + 'cookies';
+                allCookies = allCookies + hour[i];
+                totalArray[0].textContent = 'Total=' + allCookies;
+                console.log(allCookies,this.totalArray,this.hour[i])
             }
-            else{
-        this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum)*this.avrCookies;
+            else {
+                this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum) * this.avrCookies;
 
-                hour[i].textContent = (i+6) +'am:' + cookiesPerHour + 'cookies';
-                allCookies= allCookies +hour[i];
-                totalArray[0].textContent='Total='+ allCookies;
+                hour[i].textContent = (i + 6) + 'am:' + cookiesPerHour + 'cookies';
+                allCookies = allCookies + hour[i];
+                totalArray[0].textContent = 'Total=' + allCookies;
 
 
             }
@@ -36,10 +37,11 @@ var seattle = {
     },
     
 
+
 };
 // global' funtion
-    function generateRandomAge(minValue, maxValue) {
-        var randomValue = Math.random();
-        return Math.floor(randomValue * (maxValue - minValue + 1)) + minValue;
-    };
+function generateRandomAge(minValue, maxValue) {
+    var randomValue = Math.random();
+    return Math.floor(randomValue * (maxValue - minValue + 1)) + minValue;
+};
 // copy and past to add for other locations

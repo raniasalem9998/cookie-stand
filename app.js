98f7f26104b5ce521992d7af98de43aac5b43982
raniasalem9998 <rania.salem9998@gmail.com>
2020-06-14 22:54:37 +0300

    class06

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var seattle = {
+    location: 'seatle',
+    minCostumerNum: 23,
+    maxCostumerNum: 65,
+    avrCookies: 6.3,
+    hour: document.querySelectorAll("li"), //cookies in the hour
+    cookiesPerHour=0,
+    totalArray=document.querySelectorAll(".total")[0],
+
+    // randum: function () {
+    //     this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum);
+    // },
+    
+    total: function () {
+        for (var i = 0; i <= this.hour.length; i++) {
+             var allCookies=0;
+            if (i<=6){
+        this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum)*this.avrCookies;
+
+                hour[i].textContent = (i+6) +'pm:' + cookiesPerHour + 'cookies';
+                  allCookies=allCookies+hour[i];
+                totalArray[0].textContent='Total='+ allCookies;
+            }
+            else{
+        this.cookiesPerHour = generateRandomAge(this.minCostumerNum, this.maxCostumerNum)*this.avrCookies;
+
+                hour[i].textContent = (i+6) +'am:' + cookiesPerHour + 'cookies';
+                allCookies= allCookies +hour[i];
+                totalArray[0].textContent='Total='+ allCookies;
+
+
+            }
+        };
+    },
+    
+
+};
+// global' funtion
+    function generateRandomAge(minValue, maxValue) {
+        var randomValue = Math.random();
+        return Math.floor(randomValue * (maxValue - minValue + 1)) + minValue;
+    };
+// copy and past to add for other locations
